d35036ad02c38766d88bf8ec103540a15fd41e98
Stefan Fritz <stefanbfritz@gmail.com>
2021-09-26 19:55:43 -0400

    Update fullCredit.js

--- fullCredit.js
+++ fullCredit.js
@@ -1,3 +1,9 @@
 javascript: (function() {
-  document.getElementById('submit-grade-full-credit').click();
+  let abacus_tabs = document.querySelectorAll('.__abacus_tab > div');
+  abacus_tabs.forEach( elem => {
+    if (elem.textContent === "Grade") {
+      elem.click();
+      document.getElementById('submit-grade-full-credit').click();
+    }
+  });
 })();
